53b599a2b438d78191323c2fde685e75973bf4cd
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-05-12 10:54:06 -0400

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -1,13 +1,13 @@
 public class Horse {
   
-  # Variables
+  // Variables for the House
   
   public Horse(___, ___, ...) {
-    
+    // constructor
   }
   
-  public ____ winning_formuala() {
-    
+  public ____ running_formuala() {
+    // your formula to determine the amount of steps a hourse can run
   }
   
 }
